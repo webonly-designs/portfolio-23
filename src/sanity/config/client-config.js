02a39561a5745a635df44a11e0a214a2921bdabc
webonly-designs <webonly.designs@gmail.com>
2023-06-27 19:30:34 -0500

    update

--- src/sanity/config/client-config.js
+++ src/sanity/config/client-config.js
@@ -2,7 +2,6 @@ const config = {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     apiVersion: "2023-06-26",
-    useCdn: false,
   }
   
   export default config;
